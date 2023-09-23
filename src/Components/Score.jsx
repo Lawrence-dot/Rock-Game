cb1651abd0ee9cd1106c5f0ed50e775537d24fb2
Ojediran Lawrence <damilareojediran3@gmail.com>
2023-09-23 20:26:27 +0100

    g

--- src/Components/Score.jsx
+++ src/Components/Score.jsx
@@ -7,33 +7,35 @@ function Score() {
   return (
     <div className="score flex rounded-md text-white">
       <div className="scorename flex flex-col">
-        <span>
-          ROCK
-          <br />
-          PAPER
-          <br />
-          SCISSORS
-        </span>
+        <div className="rpc">
+          <div className="">ROCK</div>
+          <div className="">PAPER</div>
+          <div className="">SCISSORS</div>
+        </div>
       </div>
 
-      <div className="scoredash livesdash flex flex-col">
-        <span className="scoretext">Lives</span>
-        <span
-          className={`scorenum ${
-            homecontext.lives <= 1 ? "text-red" : "text-green"
-          }`}
-        >
-          {" "}
-          {homecontext.lives}{" "}
-        </span>
-      </div>
+      <div className="scoress">
+        <div className="scoredash livesdash flex flex-col">
+          <span className="scoretext">Lives</span>
+          <span
+            className={`scorenum ${
+              homecontext.lives <= 1 ? "text-red" : "text-green"
+            }`}
+          >
+            {" "}
+            {homecontext.lives}{" "}
+          </span>
+        </div>
 
-      <div className="scoredash flex flex-col">
-        <span className="scoretext">Score</span>
-        <span className={`scorenum ${homecontext.score > 10 && "text-green"}`}>
-          {" "}
-          {homecontext.score}{" "}
-        </span>
+        <div className="scoredash flex flex-col">
+          <span className="scoretext">Score</span>
+          <span
+            className={`scorenum ${homecontext.score > 10 && "text-green"}`}
+          >
+            {" "}
+            {homecontext.score}{" "}
+          </span>
+        </div>
       </div>
     </div>
   );
